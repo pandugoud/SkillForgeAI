cb58f57d435e9ee7e63c9f5473344c583d4d2b8f
Prathap goud <65637706+pandugoud@users.noreply.github.com>
2026-07-13 12:00:32 +0530

    Add build configuration for chunk size warning limit

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,8 +3,14 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+
   plugins: [
     react(),
     tailwindcss(),
   ],
+
+  build: {
+    chunkSizeWarningLimit: 1000,
+  },
+
 });
